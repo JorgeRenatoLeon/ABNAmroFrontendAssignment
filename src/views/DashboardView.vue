d580405cb6642ce6790f53563a1f050a09bf1340
Jorge Leon <jrleonchumpitaz@gmail.com>
2026-05-25 02:50:58 +0200

    chore: fix files formatting because of main laptop issues

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -5,13 +5,13 @@ const { isLoading, error, topRatedShows, genreSections } = useShows()
 </script>
 
 <template>
-  <main class="p-6">
-    <h1 class="text-2xl font-bold">Dashboard</h1>
-    <p v-if="isLoading">Loading shows...</p>
-    <p v-else-if="error">{{ error }}</p>
-    <div v-else>
-      <p>Top rated: {{ topRatedShows.length }}</p>
-      <p>Genres: {{ genreSections.length }}</p>
-    </div>
-  </main>
+    <main class="p-6">
+        <h1 class="text-2xl font-bold">Dashboard</h1>
+        <p v-if="isLoading">Loading shows...</p>
+        <p v-else-if="error">{{ error }}</p>
+        <div v-else>
+            <p>Top rated: {{ topRatedShows.length }}</p>
+            <p>Genres: {{ genreSections.length }}</p>
+        </div>
+    </main>
 </template>
